72371434270682038d6f41882cd66993d6297a2c
Zena-art <zena.nazim@gmail.com>
2024-09-14 22:59:25 -0500

    prime number is done

--- script.js
+++ script.js
@@ -13,18 +13,40 @@ If a number is not divisible by either 3 or 5, log the number.
 let j;
 
 
-for (let i = 1; i < 101; i++){
-  if(i % 3 === 0 && i % 5 ===0 ){
-    j = "Fizz Buzz";
-    console.log(j)
-  }else if(i % 3 === 0){
-    j = "Fizz";
-    console.log(j)
+// for (let i = 1; i < 101; i++){
+//   if(i % 3 === 0 && i % 5 ===0 ){
+//     j = "Fizz Buzz";
+//     console.log(j)
+//   }else if(i % 3 === 0){
+//     j = "Fizz";
+//     console.log(j)
+//   }
+//   else if(i % 5 === 0){
+//     j = "Buzz";
+//     console.log(j);
+//   }else{
+//   console.log(i);
+//   }
+// }
+
+//Declare an arbitrary number, n.
+//Create a loop that searches for the next prime number, starting at n and incrementing from there.
+//As soon as you find the prime number, log that number and exit the loop.
+
+let n = 180;
+let isPrime = false;
+
+while(!isPrime){
+  isPrime = true;
+  n++;
+
+  for(let i = 2; i < n / 2; i++){
+    if(n % i === 0){
+      isPrime = false;
+      break;
+    }
   }
-  else if(i % 5 === 0){
-    j = "Buzz";
-    console.log(j);
-  }else{
-  console.log(i);
+  if(isPrime){
+    console.log("Next Prime Number is: " + n )
   }
 }
